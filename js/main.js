c6302d7429e0bcb6e0eb6cb51eca312797c71518
MNxMunno <bilolhuzayfa@gmmail.com>
2024-03-20 16:54:26 +0500

    new product added

--- js/main.js
+++ js/main.js
@@ -9,5 +9,38 @@ const btnLike = document.querySelector(".btn-like");
 const like = document.querySelector(".like");
 
 btnLike.addEventListener("click", () => {
-  like.classList.toggle("red")
+  like.classList.toggle("red");
 });
+
+//////////////////////////////////////////////////////////////////////////////
+const cards = document.querySelector(".cards");
+import { products } from "./server.js";
+localStorage.setItem("products", JSON.stringify(products));
+
+const data = JSON.parse(localStorage.getItem("products"));
+// console.log(data);
+
+function createCard(data) {
+  while (cards.firstChild) {
+    cards.firstChild.remove();
+  }
+  data.forEach((user, index) => {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+    <div class="card">
+                <img src="${user.thumbnail}" alt="img">
+                <div class="card-content">
+                  <button class="sale">SALE</button>
+                  <button class="gift">🎁 Подарок</button>
+                  <b>${user.discountPercentage}</b>
+                  <h4>${user.description}</h4>
+                  <p class="price">${user.price}₽ <del>${user.stock}₽</del></p>
+                </div>
+              </div>
+    `;
+    cards.appendChild(card);
+  });
+}
+
+createCard(data);
